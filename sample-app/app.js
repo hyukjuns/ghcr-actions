98b4e580129af382318fd0ad303720c0b1be3836
hyukjun <hyukjun1994@gmail.com>
2021-01-13 11:14:42 +0900

    test4

--- sample-app/app.js
+++ sample-app/app.js
@@ -5,7 +5,7 @@ const fastify = require('fastify')({
 
 // Declare a route
 fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+  reply.send({ hello: 'world_v2' })
 })
 
 // Run the server!
